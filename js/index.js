5c53173452ef19accb5efbf2dc9e4c42b79e414f
Ilya Yalymov <chezeee@gmail.com>
2023-01-29 21:52:06 +0300

    Problem solution added

--- js/index.js
+++ js/index.js
@@ -3,10 +3,15 @@ import User from "./user.js";
 // class usage
 const sam = new User("Sam", "Blue", 49);
 console.log(sam);
-sam.getFullName();
+console.log(sam.getFullName());
+console.log(sam.getInitials());
+console.log(sam.canVote());
 
 console.log("---");
 
 const charlie = new User("Charlie", "Doe", 13);
 console.log(charlie);
 charlie.getFullName();
+console.log(charlie.getFullName());
+console.log(charlie.getInitials());
+console.log(charlie.canVote());
--- js/user.js
+++ js/user.js
@@ -1,4 +1,20 @@
 // Class definition
 export default class User {
-  // your code
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getInitials() {
+    return this.firstName[0] + this.lastName[0];
+  }
+
+  canVote() {
+    return `Can vote? - ${this.age >= 18}`;
+  }
 }
